c03f068ff4e5becaf99fb1a003a49e7bb157effe
Filipe Siqueira <fcsiqueira1409@gmail.com>
2023-07-07 11:32:39 -0300

    build: install dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      nuBg: "#591e8c",
+      nuText: "#7c5da4",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
